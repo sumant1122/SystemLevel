a99b2b49c07aeb694be0b2126b38cb68d6a26c5d
Sumant <sumant1122@gmail.com>
2025-03-19 22:56:42 +0530

    added usage bar

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,26 @@ fn main() {
         println!("Global CPU usage: {:.2}%", global_cpu_usage);
 
         for (i, cpu) in sys.cpus().iter().enumerate() {
-            println!("CPU {} usage: {:.2}%", i, cpu.cpu_usage());
+            print_cpu_usage_bar(&format!("CPU {}", i), cpu.cpu_usage());
         }
+
+        print!("\x1B[{}A", sys.cpus().len()+1);
 
         thread::sleep(Duration::from_secs(1));
     }
+}
+
+fn print_cpu_usage_bar(label: &String, usage: f32) {
+
+    let bar_width = 20;
+
+    let filled = (usage/100.0 * bar_width as f32).round() as usize;
+
+    let bar = format!(
+        "[{}{}]",
+        "=".repeat(filled),
+        " ".repeat(bar_width - filled)
+    );
+
+    println!("{}: {} {:.2}%", label, bar, usage)
 }
